6998e1931995f9aec132ee0d64c08949eee605ab
xiejj <xiejj@cstnet.cn>
2020-09-29 17:58:27 +0800

    增加审计信息

--- src/main/java/cn/arp/trend/web/biz/BasicController.java
+++ src/main/java/cn/arp/trend/web/biz/BasicController.java
@@ -1,5 +1,6 @@
 package cn.arp.trend.web.biz;
 
+import cn.arp.trend.auth.Audit;
 import cn.arp.trend.data.model.DO.AcademicianQueryDO;
 import cn.arp.trend.data.model.DO.OrgInfoQueryDO;
 import cn.arp.trend.data.model.DTO.AcademicianInfoDTO;
@@ -42,6 +43,7 @@ public class BasicController extends BaseController {
     @ApiOperation(value= "查询菜单", notes= "无参查询菜单")
     @ServiceExecuter(description = "查询菜单")
     @RequestMapping(value = "/nav", method = RequestMethod.POST)
+    @Audit("查询菜单")
     public MenuResponse menuQuery() {
         List<MenuResult> menuList = Lists.newArrayList();
         menuList.add(new MenuResult("科研投入", "/rp"));
@@ -58,6 +60,7 @@ public class BasicController extends BaseController {
     @ApiOperation(value= "查询单位信息、领域信息", notes= "查询单位信息、领域信息")
     @ServiceExecuter(description = "查询单位信息、领域信息")
     @RequestMapping(value = "/info", method = RequestMethod.POST)
+    @Audit("查询单位信息、领域信息")
     public OrgInfoResponse orgQuery(OrgInfoQueryRequest request) {
         OrgInfoQueryDO orgInfoQueryDO = OrgInfoRequestConverter.INSTANCE.domain2dto(request);
         OrgInfoDTO bizResult = basicService.orgInfoQuery(orgInfoQueryDO);
@@ -70,6 +73,7 @@ public class BasicController extends BaseController {
     @ApiOperation(value= "获取近十年的年份", notes= "获取近十年的年份，用于时间下拉菜单，供用户选择起止时间")
     @ServiceExecuter(description = "获取近十年的年份")
     @RequestMapping(value = "/year", method = RequestMethod.POST)
+    @Audit("获取近十年的年份")
     public List<String> yearQuery() {
         List<String> bizResult = basicService.yearQuery();
         return bizResult;
@@ -78,6 +82,7 @@ public class BasicController extends BaseController {
     @ApiOperation(value= "获取中科院院士学部信息，工程院院士学部信息，单位信息（两者的并集）", notes= "获取中科院院士学部信息，工程院院士学部信息，单位信息（两者的并集）")
     @ServiceExecuter(description = "获取中科院院士学部信息，工程院院士学部信息，单位信息（两者的并集）")
     @RequestMapping(value = "/academician", method = RequestMethod.POST)
+    @Audit("获取中科院院士学部信息，工程院院士学部信息，单位信息（两者的并集）")
     public AcademicianResponse academicianQuery(AcademicianQueryRequest request) {
         AcademicianQueryDO academicianQueryDO =  AcademicianRequestConverter.INSTANCE.domain2dto
                 (request);
@@ -89,6 +94,7 @@ public class BasicController extends BaseController {
     @ApiOperation(value= "国际合作头部", notes= "国际合作头部")
     @ServiceExecuter(description = "国际合作头部")
     @RequestMapping(value = "/internationInfo", method = RequestMethod.POST)
+    @Audit("国际合作头部")
     public InternationInfoResponse internationInfoQuery() {
         InternationInfoDTO internationInfoDTO = basicService.internationInfoQuery();
         return new InternationInfoResponse(
