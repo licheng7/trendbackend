0dc5afd51200c1a8d6b9ee01878c5161522782f0
david liu <18911129157@163.com>
2020-11-06 02:21:43 +0800

    great for contrast YouthTalent by unit(previous is by field)

--- src/main/java/cn/arp/trend/web/biz/ContrastYouthTalentController.java
+++ src/main/java/cn/arp/trend/web/biz/ContrastYouthTalentController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.*;
 
 /**
@@ -125,7 +127,10 @@ public class ContrastYouthTalentController extends BaseController {
             ArrayList<Double> arrayList = new ArrayList<Double>();
             for(int j = startYear ; j <= endYear ; j++)
             {
-                arrayList.add((0.0+helpStructAgeAry.get(fieldName)[j - startYear][0])/(0.0+helpStructAgeAry.get(fieldName)[j - startYear][1]));
+                double ration = (0.0+helpStructAgeAry.get(fieldName)[j - startYear][0])/(0.0+helpStructAgeAry.get(fieldName)[j - startYear][1]);
+                BigDecimal b = new BigDecimal(ration);
+                double f1 = b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+                arrayList.add(f1);
             }
             ageAryOne.put("value", arrayList);
             proportionAryAry.add(ageAryOne);
@@ -170,9 +175,9 @@ public class ContrastYouthTalentController extends BaseController {
 
         // educationAry
         HashMap<String, HashMap<String, Object>> educationAryHelper = new HashMap<String, HashMap<String, Object>>();
-        for(int i = 0;i<resList3.size();i++)
+        for(int i = 0;i<resList4.size();i++)
         {
-            HashMap<String, Object> item = resList3.get(i);
+            HashMap<String, Object> item = resList4.get(i);
 
             String fieldName = (String) item.get("research_field");
             Long rs = Long.parseLong("" + item.get("rs"));
@@ -228,6 +233,193 @@ public class ContrastYouthTalentController extends BaseController {
     @RequestMapping(value = "/unit", method = RequestMethod.POST)
     @Audit(desc="")
     public HashMap<String, Object> contrastByUnit(@RequestBody ContrastBaseRequest request) {
-        return null;
+        Calendar cal = Calendar.getInstance();
+        Integer endYear = cal.get(Calendar.YEAR) - 1;
+        Integer startYear = endYear - 9;
+        // Integer startNf = 1980;
+
+        List<HashMap<String, Object>> resList1 = contrastYouthTalentService.byUnit1(
+                request.getUserId(),
+                startYear,
+                endYear,
+                request.getDataAry());
+
+        List<HashMap<String, Object>> resList2 = contrastYouthTalentService.byUnit2(
+                request.getUserId(),
+                startYear,
+                endYear,
+                request.getDataAry());
+
+        List<HashMap<String, Object>> resList3 = contrastYouthTalentService.byUnit3(
+                request.getUserId(),
+                startYear,
+                endYear,
+                request.getDataAry());
+
+        List<HashMap<String, Object>> resList4 = contrastYouthTalentService.byUnit4(
+                request.getUserId(),
+                startYear,
+                endYear,
+                request.getDataAry());
+
+        ArrayList<Object> yearList = new ArrayList<Object>();
+        for(int j = startYear ; j <= endYear ; j++)
+        {
+            yearList.add((long) j);
+        }
+
+        if(resList1 == null || resList2 == null || resList3 == null || resList4 == null)
+        {
+            return null;
+        }
+
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        // project
+        result.put("project", resList1);
+
+        // ageAry
+        Map<String, long[][]> helpStructAgeAry = new HashMap<>();
+        for(int i = 0;i<resList2.size();i++)
+        {
+            HashMap<String, Object> item = resList2.get(i);
+            String fieldName = (String) item.get("jgmc");
+            Long rcrs = Long.parseLong("" + item.get("rcrs"));
+            Long zgrs = Long.parseLong("" + item.get("zgrs"));
+            Integer nf = Integer.parseInt(""+item.get("nf"));
+
+            if(!helpStructAgeAry.containsKey(fieldName))
+            {
+                helpStructAgeAry.put(fieldName, new long[endYear - startYear + 1][2]);
+            }
+            helpStructAgeAry.get(fieldName)[nf - startYear][0] = rcrs;
+            helpStructAgeAry.get(fieldName)[nf - startYear][1] = zgrs;
+        }
+
+        ArrayList<Object> ageAry = new ArrayList<Object>();
+        for(String fieldName : helpStructAgeAry.keySet())
+        {
+            HashMap<String, Object> ageAryOne = new HashMap<String, Object>();
+            ageAryOne.put("name", fieldName);
+
+            ArrayList<Long> arrayList = new ArrayList<Long>();
+            for(int j = startYear ; j <= endYear ; j++)
+            {
+                arrayList.add(helpStructAgeAry.get(fieldName)[j - startYear][0]);
+            }
+            ageAryOne.put("value", arrayList);
+            ageAry.add(ageAryOne);
+        }
+        result.put("ageAry", ageAry);
+
+        // proportionAry,rcrs/zgrs
+        ArrayList<Object> proportionAryAry = new ArrayList<Object>();
+        for(String fieldName : helpStructAgeAry.keySet())
+        {
+            HashMap<String, Object> ageAryOne = new HashMap<String, Object>();
+            ageAryOne.put("name", fieldName);
+
+            ArrayList<Double> arrayList = new ArrayList<Double>();
+            for(int j = startYear ; j <= endYear ; j++)
+            {
+                double ration = (0.0+helpStructAgeAry.get(fieldName)[j - startYear][0])/(0.0+helpStructAgeAry.get(fieldName)[j - startYear][1]);
+                BigDecimal b = new BigDecimal(ration);
+                double f1 = b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+                arrayList.add(f1);
+            }
+            ageAryOne.put("value", arrayList);
+            proportionAryAry.add(ageAryOne);
+        }
+        result.put("proportionAry", proportionAryAry);
+
+        // yearsAry
+        result.put("yearsAry", yearList);
+
+        // title
+        HashMap<String, HashMap<String, Object>> titleHelper = new HashMap<String, HashMap<String, Object>>();
+        for(int i = 0;i<resList3.size();i++)
+        {
+            HashMap<String, Object> item = resList3.get(i);
+
+            String fieldName = (String) item.get("jgmc");
+            Long rs = Long.parseLong("" + item.get("rs"));
+            String zwmc = "" + item.get("zwmc");
+
+            if(!titleHelper.containsKey(fieldName))
+            {
+                titleHelper.put(fieldName, new HashMap<String, Object>());
+            }
+            titleHelper.get(fieldName).put(zwmc, rs);
+        }
+        ArrayList<Object> titleList = new ArrayList<Object>();
+        for(String field:titleHelper.keySet())
+        {
+            HashMap<String, Object> one = new HashMap<String, Object>();
+            one.put("jgmc", field);
+            if(titleHelper.get(field).keySet().contains("正高级"))
+            {
+                one.put("zg", titleHelper.get(field).get("正高级"));
+            }
+            if(titleHelper.get(field).keySet().contains("副高级"))
+            {
+                one.put("fg", titleHelper.get(field).get("副高级"));
+            }
+            titleList.add(one);
+        }
+        result.put("title", titleList);
+
+        // educationAry
+        HashMap<String, HashMap<String, Object>> educationAryHelper = new HashMap<String, HashMap<String, Object>>();
+        for(int i = 0;i<resList4.size();i++)
+        {
+            HashMap<String, Object> item = resList4.get(i);
+
+            String fieldName = (String) item.get("jgmc");
+            Long rs = Long.parseLong("" + item.get("rs"));
+            String xw = "" + item.get("xw");
+
+            if(!educationAryHelper.containsKey(fieldName))
+            {
+                educationAryHelper.put(fieldName, new HashMap<String, Object>());
+            }
+            educationAryHelper.get(fieldName).put(xw, rs);
+        }
+        ArrayList<Object> educationAryList = new ArrayList<Object>();
+        for(String field:educationAryHelper.keySet())
+        {
+            HashMap<String, Object> one = new HashMap<String, Object>();
+            one.put("name", field);
+
+            ArrayList<Object> valueList = new ArrayList<>();
+            if(educationAryHelper.get(field).keySet().contains("博士"))
+            {
+                HashMap<String, Object> oneValue = new HashMap<String, Object>();
+                oneValue.put("name", "博士");
+                oneValue.put("value", educationAryHelper.get(field).get("博士"));
+                valueList.add(oneValue);
+            }
+            if(educationAryHelper.get(field).keySet().contains("硕士"))
+            {
+                HashMap<String, Object> oneValue = new HashMap<String, Object>();
+                oneValue.put("name", "硕士");
+                oneValue.put("value", educationAryHelper.get(field).get("硕士"));
+                valueList.add(oneValue);
+            }
+            one.put("value", valueList);
+            educationAryList.add(one);
+        }
+
+        result.put("educationAry", educationAryList);
+
+        // result_array
+        ArrayList<Object> allResultList = new ArrayList<Object>();
+        allResultList.add(resList1);
+        allResultList.add(resList2);
+        allResultList.add(resList3);
+        allResultList.add(resList4);
+        result.put("result_array", allResultList);
+
+        // keep format as nodejs server
+        return result;
     }
 }
